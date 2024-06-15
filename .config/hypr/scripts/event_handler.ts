7ef6e0517931cadbe9815daee170674e522c8a0a
Momijiichigo <ichigomomiji436@gmail.com>
2024-06-15 16:20:41 -0500

    update

--- .config/hypr/scripts/event_handler.ts
+++ .config/hypr/scripts/event_handler.ts
@@ -2,81 +2,77 @@
 import {$} from "bun";
 import {Some, None, Option} from "rusty-enums"
 
-import {setEwwVar as setEwwVarWspaces} from "../../eww/scripts/workspaces"
-import {setEwwVar as setEwwVarCurrentWspace} from "../../eww/scripts/current_workspace"
 
-const monitorNameMap: Map<string, number> = new Map();
 
-export type EventS2 = `${string}>>${string}\n`;
+
+
+const S2Listeners: {
+  [key: string]: Array<(data: string[]) => Promise<void>>
+} = {}
+
 /**
- * Listens to `$XDG_RUNTIME_DIR/hypr/[HIS]/.socket2.sock`
+ * Adds Listeners to `$XDG_RUNTIME_DIR/hypr/[HIS]/.socket2.sock` events.
  *
  * Spec: https://wiki.hyprland.org/IPC/#xdg_runtime_dirhyprhissocket2sock
  *
  */
-export async function handleEventS2(text: EventS2) {
-  const [event, dataStr] = text.split(">>");
-  const data = dataStr.trim().split(",");
-
-  console.log("event:", event, "data:", data)
-  switch (event) {
-    case "workspacev2": {
-      // data: [WORKSPACEID, WORKSPACENAME]
-      await $`eww update current_workspace=${data[0]}`
-        .nothrow()
-        .text();
-      break;
-    }
-    case "monitoraddedv2": {
-      // data: [MONITORID,MONITORNAME,MONITORDESCRIPTION]
-      const disp_id = data[0];
-      const disp_name = data[1];
-
-      monitorNameMap.set(disp_name, Number(disp_id));
-      await Promise.all([
-        $`eww open-many bar:${disp_name} \
-          --arg ${disp_name}:screen=${disp_id}`
-          .nothrow()
-          .text(),
-        setEwwVarWspaces()
-      ])
-      break;
-    }
-    case "monitorremoved": {
-      // data: [MONITORNAME]
-      const disp_name = data[0];
-      monitorNameMap.delete(disp_name);
-      await Promise.all([
-        $`eww close bar:${disp_name}`
-          .nothrow()
-          .text(),
-        setEwwVarWspaces()
-      ])
-      break;
-    }
-    case "createworkspace": {
-      await setEwwVarWspaces()
-      console.log("createworkspace", data)
-      break;
-    }
-    case "destroyworkspace": {
-      await setEwwVarWspaces()
-      break;
-    }
-    case "moveworkspace": {
-      await Promise.all([
-        setEwwVarWspaces(),
-        setEwwVarCurrentWspace()
-      ])
-      break;
-    }
-    case "movewindow": {
-      await setEwwVarCurrentWspace()
-      break;
-    }
-
-    default:
-      break;
+export function addS2Listener(
+  event: string,
+  listener: (data: string[]) => Promise<void>
+) {
+  if (!S2Listeners[event]) {
+    S2Listeners[event] = [];
   }
-
+  S2Listeners[event].push(listener);
 }
+
+/**
+ * Listens to the `$XDG_RUNTIME_DIR/hypr/[HIS]/.socket2.sock` events.
+ *
+ * Spec: https://wiki.hyprland.org/IPC/#xdg_runtime_dirhyprhissocket2sock
+ *
+ *
+ * Note:
+ * ```sh
+ * socat -U - UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock
+ * ```
+ */
+export async function listenToS2EventLoop() {
+
+  const { XDG_RUNTIME_DIR, HYPRLAND_INSTANCE_SIGNATURE } = Bun.env;
+  const proc = Bun.spawn([
+    "socat",
+    "-U",
+    "-",
+    `UNIX-CONNECT:${XDG_RUNTIME_DIR}/hypr/${HYPRLAND_INSTANCE_SIGNATURE}/.socket2.sock`
+  ]);
+  const reader = proc.stdout.getReader();
+
+  const decoder = new TextDecoder();
+
+  let done = false;
+  while (!done) {
+    const {value, done: d} = await reader.read()
+    done = d;
+    const text = decoder.decode(value)
+
+    console.log("text:", text)
+
+    const eventLines = text.trim().split("\n")
+
+    // eventStr: "eventName>>data1,data2,data3"
+    for (const eventStr of eventLines) {
+
+      const [event, dataStr] = eventStr.split(">>");
+      const data = dataStr.trim().split(",");
+
+      console.log("event:", event, "data:", data)
+
+      if (event in S2Listeners) {
+        await Promise.all(S2Listeners[event].map(listener => listener(data)))
+      }
+    }
+
+  }
+}
+
--- .config/hypr/scripts/index.ts
+++ .config/hypr/scripts/index.ts
@@ -3,18 +3,26 @@
 import {$} from "bun";
 import {Some, None, Option} from "rusty-enums"
 
-import {handleEventS2} from "./event_handler.ts"
-import type {EventS2} from "./event_handler.ts"
+import {setEwwVar as setEwwVarWspaces} from "../../eww/scripts/workspaces"
+import {setEwwVar as setEwwVarCurrentWspace} from "../../eww/scripts/current_workspace"
+
+import {listenToS2EventLoop, addS2Listener} from "./event_handler.ts"
 
 const arg = Bun.argv[2];
 switch (arg) {
-  case "main":
+  case "main": {
     await mainLoop();
     break;
+  }
   default:
     break;
 }
+
+
+const monitorNameMap: Map<string, number> = new Map();
+
 async function mainLoop() {
+  // Check if the process is already running
   const foundProcess = await $`ps o command | grep -E '[/ \w]+\.config/hypr/scripts/index\.ts main'`
     .nothrow()
     .text();
@@ -24,32 +32,61 @@ async function mainLoop() {
   const processNotRunning = foundProcess.trim().split('\n').length < 2;
   console.log("processNotRunning", processNotRunning)
   if (processNotRunning) {
-    // socat -U - UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock
-    const { XDG_RUNTIME_DIR, HYPRLAND_INSTANCE_SIGNATURE } = Bun.env;
-    const proc = Bun.spawn([
-      "socat",
-      "-U",
-      "-",
-      `UNIX-CONNECT:${XDG_RUNTIME_DIR}/hypr/${HYPRLAND_INSTANCE_SIGNATURE}/.socket2.sock`
-    ]);
-    const reader = proc.stdout.getReader();
 
-    const decoder = new TextDecoder();
-
-    let done = false;
-    while (!done) {
-      const {value, done: d} = await reader.read()
-      done = d;
-      const text = decoder.decode(value)
-
-      console.log("text:", text)
-      const eventStr = text.trim().split("\n") as EventS2[]
-      for (const event of eventStr) {
-        await handleEventS2(event);
-      }
-
-    }
-  
+    registerListeners();
+    await listenToS2EventLoop();
   }
 }
 
+
+function registerListeners() {
+  // Registering S2 event listeners
+
+  addS2Listener("workspacev2", async ([ws_id, _ws_name]) => {
+    await $`eww update current_workspace=${ws_id}`
+      .nothrow()
+      .text();
+  })
+
+  addS2Listener("monitoraddedv2", async ([disp_id, disp_name]) => {
+    monitorNameMap.set(disp_name, Number(disp_id));
+    await Promise.all([
+      $`eww open-many bar:bar_${disp_name} \
+          --arg bar_${disp_name}:screen=${disp_id}`
+        .nothrow()
+        .text(),
+      setEwwVarWspaces()
+    ])
+  })
+
+  addS2Listener("monitorremoved", async ([disp_name]) => {
+    monitorNameMap.delete(disp_name);
+    await Promise.all([
+      $`eww close bar_${disp_name}`
+        .nothrow()
+        .text(),
+      setEwwVarWspaces()
+    ])
+  })
+
+  addS2Listener("createworkspace", async ([ws_id, _ws_name]) => {
+    await setEwwVarWspaces()
+    console.log("createworkspace", ws_id)
+  })
+
+  addS2Listener("destroyworkspace", async () => {
+    await setEwwVarWspaces()
+  })
+
+  addS2Listener("moveworkspace", async () => {
+    await Promise.all([
+      setEwwVarWspaces(),
+      setEwwVarCurrentWspace()
+    ])
+  })
+
+  addS2Listener("movewindow", async () => {
+    await setEwwVarCurrentWspace()
+  })
+
+}
